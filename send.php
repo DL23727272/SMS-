a9e021faf7cd42acf37fcdab3dc8ce4fedb51941
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-02-17 18:34:31 +0800

    First project using SMS Twilio

--- send.php
+++ send.php
@@ -15,7 +15,7 @@ $message = $_POST["message"];
 if ($_POST["provider"] === "infobip") {
 
     $base_url = "https://your-base-url.api.infobip.com";
-    $api_key = "your API key";
+    $api_key = "SK8db44565ed8c751d92e1289916274952";
 
     $configuration = new Configuration(host: $base_url, apiKey: $api_key);
 
@@ -26,21 +26,21 @@ if ($_POST["provider"] === "infobip") {
     $message = new SmsTextualMessage(
         destinations: [$destination],
         text: $message,
-        from: "daveh"
+        from: "Paesano"
     );
 
     $request = new SmsAdvancedTextualRequest(messages: [$message]);
 
     $response = $api->sendSmsMessage($request);
 
-} else {   // Twilio
+} else {  
 
-    $account_id = "your account SID";
-    $auth_token = "your auth token";
+    $account_id = "ACf020387fb78543a29960650089ea24cc";
+    $auth_token = "82045a16c6a63656cf811d04b101c95a";
 
     $client = new Client($account_id, $auth_token);
 
-    $twilio_number = "+ your outgoing Twilio phone number";
+    $twilio_number = "+18324123450";
 
     $client->messages->create(
         $number,
